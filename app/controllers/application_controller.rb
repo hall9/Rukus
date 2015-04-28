ae5c1ab92b021e40331585cc40f93240bdfc8d9e
Alex Hall <Alexander.D.Hall13.civ@mail.mil>
2015-04-28 16:22:07 -0400

    Fixed the bug when logged it, it now brings you do the feed page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
+  def after_sign_in_path_for(resource)
+    feed_path
+  end
 
   #********************
   #THIS NEEDS TO BE UNCOMMENTED in the future for security purposes
