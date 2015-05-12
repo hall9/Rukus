39bd410c5f00bf813a47f0f300e06b0adc71dc17
Alexander Wilson <acwilso15@gmail.com>
2015-05-12 10:09:17 -0400

    added upload photo form, still need to add in posting capability

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20150426201511) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "iptests", force: :cascade do |t|
+    t.string   "ipaddress"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.text     "lat"
